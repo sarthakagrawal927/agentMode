bfeecf9f14d79a020a0e8ea482000d3be70f38c7
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-02-22 03:09:17 +0530

    Remove subreddit route loading skeletons

--- web/src/app/r/[subreddit]/loading.tsx
+++ web/src/app/r/[subreddit]/loading.tsx
@@ -1,7 +1,3 @@
 export default function Loading() {
-  return (
-    <main className="container mx-auto p-8">
-      <div className="h-6 w-32 bg-muted animate-pulse rounded" />
-    </main>
-  );
+  return null;
 }
